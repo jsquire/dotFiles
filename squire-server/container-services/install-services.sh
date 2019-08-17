ec834ffa9f906ea8c837d2b617cdf011568cd79f
Jesse Squire <jesse.squire@gmail.com>
2019-08-17 17:07:56 -0400

    Adding baseline requirements for pihole logging/permissions.

--- squire-server/container-services/install-services.sh
+++ squire-server/container-services/install-services.sh
@@ -5,10 +5,27 @@ CONTAINER_SERVICES_ROOT="$1"
 if [ ${#CONTAINER_SERVICES_ROOT} -lt 1 ];
 then
   CONTAINER_SERVICES_ROOT=$PWD
+  echo "No container services root supplied, using the current directory: ${CONTAINER_SERVICES_ROOT}"
+fi
+
+PIHOLE_ROOT="$2"
+
+if [ ${#PIHOLE_ROOT} -lt 1 ];
+then
+  PIHOLE_ROOT=$PWD
+  echo "No pi-holes root supplied, using the current directory: ${PIHOLE_ROOT}"
 fi
 
 # Make the scripts executable.
 chmod +x ${CONTAINER_SERVICES_ROOT}/*.sh
+
+# Setup PiHole requirements
+mkdir -p ${PIHOLE_ROOT}/log
+mkdir -p ${PIHOLE_ROOT}/root
+
+rm -rf ${PIHOLE_ROOT}/log/pihole.log
+touch ${PIHOLE_ROOT}/log/pihole.log
+chmod 666 ${PIHOLE_ROOT}/log/pihole.log
 
 # Schedule a weekly job to update the containers.
 UPDATE_CMD=${CONTAINER_SERVICES_ROOT}/restart-update.sh
@@ -16,8 +33,8 @@ UPDATE_JOB="0 03 * * SAT root $UPDATE_CMD"
 
 ( crontab -l | grep -v -F "$UPDATE_CMD" ; echo "$UPDATE_JOB" ) | crontab -
 
-# Start the containers if not active.bash 
+# Start the containers if not active.bash
 if [ "$(docker ps | grep cloudflared |  awk '{print $2}')" != "squire/cloudflared" ]
 then
     ${CONTAINER_SERVICES_ROOT}/start-services.sh
-fi 
+fi
